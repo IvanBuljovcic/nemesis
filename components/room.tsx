1ad83ad7a6c248f78e97bef8c4e5885422a613cb
Ivan Buljovcic <ivan.buljovcic@gmail.com>
2025-02-25 23:34:24 +0100

    room updated

--- components/room.tsx
+++ components/room.tsx
@@ -1,4 +1,4 @@
-import cn from "classnames";
+import { cn } from "@/lib/utils";
 
 export type RoomProps = {
   title: string;
@@ -12,23 +12,29 @@ export default function Room({ title, explored, items, malfunctioning, onFire }:
   const roomClass = cn(
     "relative flex justify-around items-center w-full text-center text-slate-600 aspect-square flex-col overflow-hidden",
     {
-      ["bg-yellow-400"]: !explored,
-      ["bg-primary"]: !onFire && explored,
-      ["bg-red-500"]: onFire,
-      ["after:top-0 after:right-0 after:absolute after:bg-slate-500 after:w-full after:h-2 after:rotate-45 after:translate-x-1/3"]:
+      "bg-room-unexplored": !explored,
+      "bg-room-explored": !onFire && explored,
+      "bg-room-onFire": onFire,
+      "after:top-0 after:right-0 after:absolute after:bg-slate-500 after:w-full after:h-2 after:rotate-45 after:translate-x-1/3":
         malfunctioning && explored,
     }
   );
 
   if (!explored) {
-    return <div className={roomClass} />;
+    return <div className={roomClass} data-testid="wrapper-unexplored" />;
   }
 
+  const malfunctioningIndicator = () => <div data-testid="malfunctioning-indicator">Malfunctioning</div>;
+  const fireIndicator = () => <div data-testid="fire-indicator">On fire</div>;
+
   return (
-    <div className={roomClass}>
+    <div className={roomClass} data-testid="wrapper-explored">
       <h1>{title}</h1>
 
       {!!items && `Items: ${items}`}
+
+      {!!malfunctioning && malfunctioningIndicator()}
+      {!!onFire && fireIndicator()}
     </div>
   );
 }
